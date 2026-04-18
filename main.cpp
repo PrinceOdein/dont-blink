f74631ba9e46678e984476f3491ba3950f65f56a
Tamunodein Anyanwu <odeinanyanwu@gmail.com>
2026-04-18 03:52:47 +0100

    Screen Init

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-	const int screenWidth = 800;
-	const int screenHeight = 900;
+	const int screenWidth = 1900;
+	const int screenHeight = 1000;
 
 	InitWindow(screenWidth, screenHeight, "Don't Blink");
 
